5e546c054a96eb3304de383149601657728745d6
AliKhan <khan.ali0b@gmail.com>
2024-10-05 22:42:51 -0230

    Update server.ts
    server file changed back to same

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 var cors = require('cors')
 var app = express()
-app.use(cors())
+//app.use(cors())
 const ContactUs = require('./models/ContactUs');
 import mongoose from 'mongoose';
 import { MONGO_URI } from './models/config';
-//app.use(express.json());
+app.use(express.json());
 
 
 
